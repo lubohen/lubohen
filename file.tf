04f47cf4b3cef94d66d2d1f70c6a29d43c68d9e7
lubohen <lbhenrique.infonegocios@gmail.com>
2024-05-04 21:16:33 -0300

    Sample tf

---
+++ file.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "example-bucket-name"
+  acl    = "private"
+
+  tags = {
+    Name        = "ExampleBucket"
+    Environment = "Production"
+  }
+}
